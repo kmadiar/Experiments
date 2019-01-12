c52a5ef457f4afb5095640d7330fdf935fdbd6a1
Kostiantyn Madiar <>
2019-01-12 22:39:25 +0200

    added optional

---
+++ Optional/MyPlayground.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+var str = "Hello, Optional"
+
+func display(_ value: Any) {
+    print("value is - \(value)")
+}
+let noneValue = "EMPTY"
+
+var x: Int? = 1
+x = nil
+
+print(x ~= nil)
+print(x ~= 1)
+
+if x == 1 {
+    print(x)
+}
+x = 1
+print(x.unsafelyUnwrapped)
+x = nil
+
+func whatIsOptional(_ value: Optional<Any>) {
+    switch value {
+    case .some(let v):
+        display(v)
+    case .none:
+        display(noneValue)
+    }
+    
+    switch value {
+    case .some(let v):
+        display(v)
+    case nil:
+        display(noneValue)
+    }
+    
+    switch value {
+    case let z:
+        display(z)
+    case .none:
+        display(noneValue)
+    case .some(let y):
+        display(y)
+    }
+}
+
+
+whatIsOptional(x)
+x = 2
+whatIsOptional(x)
